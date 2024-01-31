2d957741e8db596a4404bbff3ca92191788da070
Rob Stanford <me@robstanford.com>
2024-01-31 15:40:31 +0000

    new recalidate

--- app/server-components/revalidate-fetch/page.js
+++ app/server-components/revalidate-fetch/page.js
@@ -1,6 +1,6 @@
 async function getData() {
   const res = await fetch(`https://strangerthings-quotes.vercel.app/api/quotes`, {
-    next: { revalidate: 20 },
+    next: { revalidate: 300 },
   });
   return res.json();
 }
